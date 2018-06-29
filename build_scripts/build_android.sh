5e841f7a2315225de96b33f2c6eebf236aec1e11
Mengjintao <meng.jintao@gmail.com>
2018-06-29 12:17:28 +0800

    sgemm.cpp has no armv7 intrisic instruction support, thus we disable armv7 compiler options in build_scripts/build_android.sh

--- build_scripts/build_android.sh
+++ build_scripts/build_android.sh
@@ -9,12 +9,12 @@ make -j4
 make install
 popd
 
-mkdir -p armeabi-v7a
-pushd armeabi-v7a
-cmake -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_ABI="armeabi-v7a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-16 -DFEATHER_ARM=1 ../..
-make -j4
-make install
-popd
+#mkdir -p armeabi-v7a
+#pushd armeabi-v7a
+#cmake -DCMAKE_TOOLCHAIN_FILE=$NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_ABI="armeabi-v7a" -DANDROID_ARM_NEON=ON -DANDROID_PLATFORM=android-16 -DFEATHER_ARM=1 ../..
+#make -j4
+#make install
+#popd
 
 #mkdir -p armeabi
 #pushd armeabi
@@ -30,8 +30,8 @@ mkdir -p include/feather
 cp ../arm64-v8a/install/feather/include/* ./include/feather/
 mkdir -p arm64-v8a
 cp ../arm64-v8a/install/feather/lib/* ./arm64-v8a/
-mkdir -p armeabi-v7a
-cp ../armeabi-v7a/install/feather/lib/* ./armeabi-v7a/
+#mkdir -p armeabi-v7a
+#cp ../armeabi-v7a/install/feather/lib/* ./armeabi-v7a/
 #mkdir -p armeabi
 #cp ../armeabi/install/feather/lib/* ./armeabi/
 #popd 
