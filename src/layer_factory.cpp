e62bfb6970f48b7b40f0158fed2b039b9cd7be54
haidonglan <turbo0628g@gmail.com>
2018-07-05 21:19:34 +0800

    I don't care about memory, seems that I should give users an option.

--- src/layer_factory.cpp
+++ src/layer_factory.cpp
@@ -57,7 +57,7 @@ Layer *GetConvolutionLayer(const LayerParameter *layer_param, const RuntimeParam
     size_t input_channels = layer_param->blobs()->Get(0)->channels();
     size_t output_channels = layer_param->blobs()->Get(0)->num();
     ConvLayer *conv_layer = NULL;
-    if (group == 1 && kernel_height == 3 && kernel_width == 3 && stride_height == 1 && stride_width == 1 && input_channels > 0 && output_channels < 512)
+    if (group == 1 && kernel_height == 3 && kernel_width == 3 && stride_height == 1 && stride_width == 1 && input_channels > 0 && output_channels < 1024)
     {
 //	printf("F63\n");
 #if 0
