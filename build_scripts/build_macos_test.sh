a32fdee85a7e551dc6eeac4cd9ae53984f5f481f
haidonglan <haidonglan@tencent.com>
2018-09-17 22:06:35 +0800

    Add macos build scripts

---
+++ build_scripts/build_macos_test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./build_scripts/build_macos_avx.sh
+clang++ ./test/test_txt.cpp -I./build-macos-avx/install/feather/include/ -L ./build-macos-avx/install/feather/lib/ -lfeather  -O3 -o feather_test
