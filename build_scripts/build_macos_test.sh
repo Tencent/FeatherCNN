cc6149745277bebd0249e1237a6638cd11231172
turbo0628 <turbo0628g@gmail.com>
2018-09-17 14:05:38 +0000

    Update macos build scripts

--- build_scripts/build_macos_test.sh
+++ build_scripts/build_macos_test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 ./build_scripts/build_macos_avx.sh
-clang++ ./test/test_txt.cpp -I./build-macos-avx/install/feather/include/ -L ./build-macos-avx/install/feather/lib/ -lfeather  -O3 -o feather_test
+clang++ ./test/test_txt.cpp -I./build-macos-avx/install/feather/include/ -L ./build-macos-avx/install/feather/lib/ -lfeather  -O3 -o feather_test_txt
+clang++ ./test/test_bin.cpp -I./build-macos-avx/install/feather/include/ -L ./build-macos-avx/install/feather/lib/ -lfeather  -O3 -o feather_test_bin
