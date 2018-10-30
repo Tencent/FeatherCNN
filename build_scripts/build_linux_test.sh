c433848d195c787b28467ab065468a90c0698768
turbo0628 <turbo0628g@gmail.com>
2018-10-30 10:42:30 +0800

    Migrate build scripts

--- build_scripts/build_linux_test.sh
+++ build_scripts/build_linux_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./build_scripts/build_linux_avx.sh
-g++ ./test/test_txt.cpp -fopenmp -I./build-linux-avx/install/feather/include/ -L ./build-linux-avx/install/feather/lib/ -lfeather  -g -o feather_test_txt
-g++ ./test/test_bin.cpp -fopenmp -I./build-linux-avx/install/feather/include/ -L ./build-linux-avx/install/feather/lib/ -lfeather  -g -o feather_test_bin
+g++ ./test/feather/test_txt.cpp -fopenmp -I./build-linux-avx/install/feather/include/ -L ./build-linux-avx/install/feather/lib/ -lfeather  -g -o feather_test_txt
+g++ ./test/feather/test_bin.cpp -fopenmp -I./build-linux-avx/install/feather/include/ -L ./build-linux-avx/install/feather/lib/ -lfeather  -g -o feather_test_bin
