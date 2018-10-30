c602756bb74c8ee2f1111c8b1326821b221cf56c
Haidong Lan <haidonglan@tencent.com>
2018-10-30 10:27:26 +0800

    Fix android test compilation link error

--- test/jni/Android.mk
+++ test/jni/Android.mk
@@ -3,8 +3,10 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := test
-LOCAL_SRC_FILES := ../test.cpp
-LOCAL_CPPFLAGS := -std=c++11 -Wall -fPIE  -I../build-android/feather/include/feather       # whatever g++ flags you like
-LOCAL_LDLIBS := -fopenmp -L$(SYSROOT)/usr/lib -llog -fPIE -pie -L../build-android/feather/armeabi-v7a -lfeather # whatever ld flags you like
+LOCAL_SRC_FILES := ../test_bin.cpp 
+LOCAL_CPPFLAGS := -std=c++11 -Wall -fPIE  -I../build-android/feather/include/feather
+LOCAL_LDLIBS := -fopenmp -L$(SYSROOT)/usr/lib -llog -fPIE -pie -latomic
+#LOCAL_LDLIBS += -L../build-android/feather/armeabi-v7a -lfeather
+LOCAL_LDLIBS += -L../build-android/feather/arm64-v8a -lfeather
 
 include $(BUILD_EXECUTABLE)
