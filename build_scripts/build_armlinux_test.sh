d629afb0605bfb834fc4f3e171ddeba438c5ca53
Haidong Lan <haidonglan@tencent.com>
2018-10-30 10:42:30 +0800

    Migrate build scripts

--- build_scripts/build_armlinux_test.sh
+++ build_scripts/build_armlinux_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-aarch64-linux-gnu-g++ ./test/test.cpp -I./build-linux-aarch64/install/feather/include/ -L ./build-linux-aarch64/install/feather/lib/ -lfeather -fopenmp -O3 -o feather_benchmark
+aarch64-linux-gnu-g++ ./test/feather/test_txt.cpp -I./build-linux-aarch64/install/feather/include/ -L ./build-linux-aarch64/install/feather/lib/ -lfeather -fopenmp -O3 -o feather_benchmark
+aarch64-linux-gnu-g++ ./test/feather/test_bin.cpp -I./build-linux-aarch64/install/feather/include/ -L ./build-linux-aarch64/install/feather/lib/ -lfeather -fopenmp -O3 -o feather_benchmark
