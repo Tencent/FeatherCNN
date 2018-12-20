97ca3a13299634bf7e2436fc381d314a37df86ec
naiyong ao <aonaiyong@gmail.com>
2018-12-20 16:37:05 +0800

    Update feather_test.

--- test/feather/jni/Application.mk
+++ test/feather/jni/Application.mk
@@ -1,7 +1,6 @@
-#APP_ABI := armeabi-v7a
 #NDK_TOOLCHAIN_VERSION := clang++
-APP_ABI := arm64-v8a
+APP_ABI := armeabi-v7a # arm64-v8a
 APP_CPPFLAGS += -std=c++11
 APP_STL := c++_static
-# APP_STL := gnustl_shared
+#APP_STL := gnustl_shared
 APP_PLATFORM := android-22
