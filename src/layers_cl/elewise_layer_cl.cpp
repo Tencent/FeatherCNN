971e3ca98af18594bf812bcab2f8d41734f807f0
piaoyi <piaoyi0208@qq.com>
2019-01-03 20:06:43 +0800

    add tuner elewise

--- src/layers_cl/elewise_layer_cl.cpp
+++ src/layers_cl/elewise_layer_cl.cpp
@@ -262,15 +262,115 @@ int EltwiseLayerCL<Dtype>::ForwardCL()
          this->name().c_str(), cl_program_name.c_str(), submit_kerel_time, start_kerel_time, stop_kerel_time);
 
 #else
-    int error_num = this->rt_param->command_queue().enqueueNDRangeKernel(
+    std::string key_gws = this->name() + "_" + "eltwise" + "_gws";
+    std::string key_lws = this->name() + "_" + "eltwise" + "_lws";
+    if (clhpp_feather::IsTuning())
+    {
+        //warm up
+        int error_num = this->rt_param->command_queue().enqueueNDRangeKernel(
                         cl_kernel, cl::NullRange, cl::NDRange(eltwise_gws[0], eltwise_gws[1], eltwise_gws[2]),
                         cl::NDRange(eltwise_lws[0], eltwise_lws[1], eltwise_lws[2]), nullptr, nullptr);
-    if (!checkSuccess(error_num))
-    {
-        LOGE("Failed enqueuing the element wise kernel.");
-        return -1;
-    }
+        if (!checkSuccess(error_num))
+        {
+            LOGE("Failed enqueuing the element wise kernel.");
+            return -1;
+        }
+        //run
+        std::vector<std::vector<size_t> > gws_list;
+        std::vector<std::vector<size_t> > lws_list;
+        gws_list.push_back(eltwise_gws);
+        lws_list.push_back(eltwise_lws);
+        uint64_t kwg_size = 0;
+        this->rt_param->cl_runtime()->GetKernelMaxWorkGroupSize(cl_kernel, kwg_size);
+        this->rt_param->cl_runtime()->tuner().TunerArry(kwg_size, this->output_height, this->output_width,
+                                 eltwise_gws, eltwise_lws, gws_list, lws_list);
+        double opt_time = std::numeric_limits<double>::max();
+        int min_tune = -1;
+        for (int j = 0; j < gws_list.size(); j++)
+        {
+            this->rt_param->command_queue().finish();
+            timespec tpstart, tpend;
+            clock_gettime(CLOCK_MONOTONIC, &tpstart);
+            int error_num = this->rt_param->command_queue().enqueueNDRangeKernel(
+                                cl_kernel, cl::NullRange, cl::NDRange(gws_list[j][0], gws_list[j][1], gws_list[j][2]),
+                                cl::NDRange(lws_list[j][0], lws_list[j][1], lws_list[j][2]), nullptr, nullptr);
+            if (!checkSuccess(error_num))
+            {
+                LOGE("Failed enqueuing the conv kernel.");
+                return -1;
+            }
 
+            this->rt_param->command_queue().finish();
+            clock_gettime(CLOCK_MONOTONIC, &tpend);
+            double timedif = 1000000.0 * (tpend.tv_sec - tpstart.tv_sec) + (tpend.tv_nsec - tpstart.tv_nsec) / 1000.0;
+            timedif /= 1000.0;
+            //LOGI("tuner kernel_name [elewise] tuner %d cost %.3f ms", j, timedif);
+            if (timedif < opt_time)
+            {
+                opt_time = timedif;
+                min_tune = j;
+            }
+        }
+        
+        this->rt_param->cl_runtime()->tuner().set_layer_kernel_wks(key_gws, gws_list[min_tune], opt_time);
+        this->rt_param->cl_runtime()->tuner().set_layer_kernel_wks(key_lws, lws_list[min_tune], opt_time);
+        //LOGI("tuner layer_name %s %s min_tune [%d]",layer_name.c_str(), key_gws.c_str(), min_tune);
+    }
+    else if (clhpp_feather::IsTunned())
+    {
+        std::vector<size_t> tmp_gws;
+        std::vector<size_t> tmp_lws;
+        this->rt_param->cl_runtime()->tuner().get_layer_kernel_wks(key_gws, tmp_gws);
+        this->rt_param->cl_runtime()->tuner().get_layer_kernel_wks(key_lws, tmp_lws);
+        int error_num = this->rt_param->command_queue().enqueueNDRangeKernel(
+                            cl_kernel, cl::NullRange, cl::NDRange(tmp_gws[0], tmp_gws[1], tmp_gws[2]),
+                            cl::NDRange(tmp_lws[0], tmp_lws[1], tmp_lws[2]), nullptr, nullptr);
+        if (!checkSuccess(error_num))
+        {
+            LOGE("Failed enqueuing the conv kernel.");
+            return -1;
+        }
+    }
+    else if (clhpp_feather::IsTunerInProcess())
+    {
+        //run
+        std::vector<std::vector<size_t> > gws_list;
+        std::vector<std::vector<size_t> > lws_list;
+        gws_list.push_back(eltwise_gws);
+        lws_list.push_back(eltwise_lws);
+        uint64_t kwg_size = 0;
+        this->rt_param->cl_runtime()->GetKernelMaxWorkGroupSize(cl_kernel, kwg_size);
+        this->rt_param->cl_runtime()->tuner().TunerArry(kwg_size, this->output_height, this->output_width,
+                                 eltwise_gws, eltwise_lws, gws_list, lws_list);
+        this->rt_param->command_queue().finish();
+        timespec tpstart, tpend;
+        clock_gettime(CLOCK_MONOTONIC, &tpstart);
+        int j = 0;
+        int error_num = this->rt_param->command_queue().enqueueNDRangeKernel(
+                            cl_kernel, cl::NullRange, cl::NDRange(gws_list[j][0], gws_list[j][1], gws_list[j][2]),
+                            cl::NDRange(lws_list[j][0], lws_list[j][1], lws_list[j][2]), nullptr, nullptr);
+        if (!checkSuccess(error_num))
+        {
+            LOGE("Failed enqueuing the conv kernel.");
+            return -1;
+        }
+        this->rt_param->command_queue().finish();
+        clock_gettime(CLOCK_MONOTONIC, &tpend);
+        double timedif = 1000000.0 * (tpend.tv_sec - tpstart.tv_sec) + (tpend.tv_nsec - tpstart.tv_nsec) / 1000.0;
+        timedif /= 1000.0;
+        this->rt_param->cl_runtime()->tuner().set_layer_kernel_wks(key_gws, gws_list[j], timedif);
+        this->rt_param->cl_runtime()->tuner().set_layer_kernel_wks(key_lws, lws_list[j], timedif);
+    }
+    else {
+        int error_num = this->rt_param->command_queue().enqueueNDRangeKernel(
+                        cl_kernel, cl::NullRange, cl::NDRange(eltwise_gws[0], eltwise_gws[1], eltwise_gws[2]),
+                        cl::NDRange(eltwise_lws[0], eltwise_lws[1], eltwise_lws[2]), nullptr, nullptr);
+        if (!checkSuccess(error_num))
+        {
+            LOGE("Failed enqueuing the element wise kernel.");
+            return -1;
+        }
+    }  
 #endif
 
     return 0;
