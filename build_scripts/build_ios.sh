dd864f7a0b77bfb6543a7d0bb44799329a16765b
jay2002 <wantong1017@163.com>
2019-03-04 15:28:51 +0800

    fixed bugs in build_ios.sh (armv7/x86_64/i386 are not fixed yet)

--- build_scripts/build_ios.sh
+++ build_scripts/build_ios.sh
@@ -5,38 +5,38 @@ mkdir -p build-ios
 pushd build-ios
 mkdir -p arm64 
 pushd arm64
-cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path) -DIOS_ARCH=arm64 -DFEATHER_ARM=1 ../..
+cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path) -DIOS_ARCH=arm64 -DBOOSTER_ARM=1 ../..
 make -j4
 make install
 popd
 
 mkdir -p armv7s
 pushd armv7s
-cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path) -DIOS_ARCH=armv7s -DFEATHER_ARM=1 ../..
+cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path) -DIOS_ARCH=armv7s -DBOOSTER_ARM=1 ../..
 make -j4
 make install
 popd
 
-mkdir -p armv7
-pushd armv7
-cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path) -DIOS_ARCH=armv7 ../..
-make -j4
-make install
-popd
+#mkdir -p armv7
+#pushd armv7
+#cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphoneos --show-sdk-path) -DIOS_ARCH=armv7 ../..
+#make -j4
+#make install
+#popd
 
-mkdir -p x86_64
-pushd x86_64
-cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphonesimulator --show-sdk-path) -DIOS_ARCH=x86_64 ../..
-make -j4
-make install
-popd
+#mkdir -p x86_64
+#pushd x86_64
+#cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphonesimulator --show-sdk-path) -DIOS_ARCH=x86_64 ../..
+#make -j4
+#make install
+#popd
 
-mkdir -p i386
-pushd i386
-cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphonesimulator --show-sdk-path) -DIOS_ARCH=i386 ../..
-make -j4
-make install
-popd
+#mkdir -p i386
+#pushd i386
+#cmake -DCMAKE_TOOLCHAIN_FILE=../../build_scripts/ios.toolchain.cmake -DIOS_SDK_PATH=$(xcrun --sdk iphonesimulator --show-sdk-path) -DIOS_ARCH=i386 ../..
+#make -j4
+#make install
+#popd
 
 popd
 bash ./build_scripts/pack_ios_framework.sh
